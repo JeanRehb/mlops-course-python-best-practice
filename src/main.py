64689cd7da96f5b5f96d220e2260e73394d0c54d
JeanRehb <jean.rehbinder@gmail.com>
2024-06-06 18:11:21 +0200

    session 3 MLOPS : refactor et docker

--- src/main.py
+++ src/main.py
@@ -3,25 +3,26 @@ from PIL import Image
 import torch
 from torchvision import transforms, models
 from torchvision.models.resnet import ResNet18_Weights
+from torch.utils.tensorboard import SummaryWriter
 
 
-class imageData:
-    def __init__(self, DIR):
-        self.D = DIR
+class ImageLoader:
+    def __init__(self, image_dir):
+        self.D = image_dir
 
-    def LoadImages(self):
+    def load_images(self):
         imgs = []
         for F in os.listdir(self.D):
-            if F.endswith(".jpg") or F.endswith(".png"):
+            if F.endswith(".jpg") or F.endswith(".png") or F.endswith(".JPG"):
                 imgs.append(Image.open(os.path.join(self.D, F)))
         return imgs
 
 
-class imgProcess:
+class ImageProcessor:
     def __init__(self, size):
         self.s = size
 
-    def resize_and_GRAY(self, img_list):
+    def apply_preprocessing(self, img_list):
         """
 
         Args:
@@ -46,26 +47,34 @@ class imgProcess:
         return p_images
 
 
-class predictor:
+class Resnet18Predictor:
     def __init__(self):
         self.mdl = models.resnet18(weights=ResNet18_Weights.DEFAULT)
         self.mdl.eval()
 
-    def Predict_Img(self, processed_images):
+    def predict(self, tensor_list):
         results = []
-        for img_tensor in processed_images:
+        for img_tensor in tensor_list:
             pred = self.mdl(img_tensor.unsqueeze(0))
             results.append(torch.argmax(pred, dim=1).item())
         return results
 
 
 if __name__ == "__main__":
-    loader = imageData("images/")
-    images = loader.LoadImages()
+    writer = SummaryWriter("tensorboard/runs/image_classification")
 
-    processor = imgProcess(256)
-    processed_images = processor.resize_and_GRAY(images)
+    loader = ImageLoader(image_dir="data/")
+    images = loader.load_images()
 
-    pred = predictor()
-    results = pred.Predict_Img(processed_images)
-    print(results)
+    processor = ImageProcessor(256)
+    preprocessed_tensor = processor.apply_preprocessing(images)
+
+    predictor = Resnet18Predictor()
+    results = predictor.predict(tensor_list=preprocessed_tensor)
+
+    for i, tensor in enumerate(preprocessed_tensor):
+        writer.add_image(f"{results[i]}", tensor, 0)
+
+    writer.close()
+
+    print(f"Predicted {len(results)} images: {results}")
